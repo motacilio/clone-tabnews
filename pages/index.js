b657a22f245d4c1cc0c8c0471991221bb704fabe
Marcos Otacílio <marcosotacilio1@gmail.com>
2024-08-23 04:58:21 +0000

    criação dos scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Eu gosto de gatos rebaixados e laranjas</h1>
+function Home() {
+  return <h1>Eu gosto de gatos rebaixados e laranjas</h1>;
 }
 
+function foda() {
+  console.log("Tmnc");
+}
 export default Home;
