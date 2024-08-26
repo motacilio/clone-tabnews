73f2ffe1323163136c109eb3be83a191d7dae82e
Marcos Otacílio <marcosotacilio1@gmail.com>
2024-08-26 16:40:53 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(response, request) {
+import database from "../../../../infra/database.js";
+
+async function status(response, request) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   request.status(200).json({ Chave: "Sexo bom é no guanabará" });
 }
 
