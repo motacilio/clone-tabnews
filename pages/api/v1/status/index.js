147ac3dde40d08c264320e2123a930dfc6a44f88
Marcos Otacílio <marcosotacilio1@gmail.com>
2024-08-27 16:23:35 +0000

    .env mv to .env.development and `jsconfig.json` add

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(response, request) {
   const result = await database.query("SELECT 1 + 1 as sum;");
